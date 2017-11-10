6a0dffc9a0d198f4dd8ed74197f4b4e5c4c2d411
loj3072 <gaya357925@gmail.com>
2017-11-10 12:01:55 +0900

    (에러나는 거)

--- src/lesson5/Banker.java
+++ src/lesson5/Banker.java
@@ -3,9 +3,9 @@ package lesson5;
 public class Banker {
     public boolean requestDeposit(DepositRequest req){ //입금요청 -수락이 됐는지 안됐는지 boolean으로 // 실제로는 그 결과값도 타입으로 해야함
         Account account = req.getDepositAccount();
-        if (!account.isDepositable()) return false;
+        if(!account.isDepositable()) return false;
         Money money = req.getMoney();
-        if (Banker.isMinimumLimit(money)) return false;
+        if(Banker.isMinimumLimit(money)) return false;
         return true;
     }
 
